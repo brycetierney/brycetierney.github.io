0bfb72fcf222ff68868dc18c4e1624a0c9ab236d
brycetierney <161519956+brycetierney@users.noreply.github.com>
2024-03-05 13:29:41 -0600

    new game idea
    changing idea from old jumping thing to bowser dodging peas game

--- Scene/sketch.js
+++ Scene/sketch.js
@@ -6,11 +6,11 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let x;
-let y;
-// let dx = 2;
-// let dy = 2;
-let mario;
+let x = 0;
+let y = 0;
+let squareSize = 65;
+let speed = 7;
+let state = "right";
 
 function setup() {
   createCanvas(400, 400);
@@ -18,15 +18,40 @@ function setup() {
 
 function draw() {
   background(220);
-}
-
-function preload() {
-  mario = loadImage("mario.png");
-}
-function moveToMouse() {
-  if (mouseIsPressed) {
-    image(mario, mouseX, mouseY);
+  
+  if (state === "right") {
+    x += speed;
+    if (x + squareSize >= width) {
+      state = "down";
+      x = width - squareSize;
+    }
   }
+  
+  else if (state === "down") {
+    y += speed;
+    if (y + squareSize >= height) {
+      state = "left";
+      y = height - squareSize;
+    }
+  }
+  
+  else if (state === "left") {
+    x -= speed;
+    if (x <= 0) {
+      state = "up";
+      x = 0;
+    }
+  }
+ 
+  else if (state === "up") {
+    y -= speed;
+    if (y <= 0) {
+      state = "right";
+      y = 0;
+    }
+  }
+  
+  square(x, y, squareSize);
 }
 
 // function everythingDown() {}
