6507c16cf0a10e73f38d4aeec28622dfbf5c2a9d
brycetierney <161519956+brycetierney@users.noreply.github.com>
2024-03-18 13:52:53 -0600

    Update sketch.js

--- terrain/sketch.js
+++ terrain/sketch.js
@@ -1,6 +1,6 @@
 // Terrain Generation
 // Object Notation and Arrays Demo
-// March 18 2024
+// March 18, 2023
 
 let terrain = [];
 let numberOfRects;
@@ -8,15 +8,27 @@ let rectWidth;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  numberOfRects = 20;
+  numberOfRects = width;
   rectWidth = width / numberOfRects;
-  spawnRectangle(200, 200);
+  generateTerrain();
 }
 
 function draw() {
   background(220);
+
   for (let someRect of terrain) {
     rect(someRect.x, someRect.y, someRect.w, someRect.h);
+  }
+}
+
+function generateTerrain() {
+  let time = 0;
+  let deltaTime = 0.003;
+
+  for (let x = 0; x < width; x += rectWidth) {
+    let theHeight = noise(time) * height;
+    spawnRectangle(x, theHeight);
+    time += deltaTime;
   }
 }
 
