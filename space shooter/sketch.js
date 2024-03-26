2bda5f4a8193b8dee282fbd5bdb4042e91333c74
brycetierney <161519956+brycetierney@users.noreply.github.com>
2024-03-26 14:10:45 -0600

    Update sketch.js

--- space shooter/sketch.js
+++ space shooter/sketch.js
@@ -1,15 +1,22 @@
-// Project Title
-// Your Name
-// Date
+// Arrays and Object Notation Assignment
+// Bryce Tierney
+// April 8th, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let radius = 60;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+function spawnShooter() {
+  circle(width/2, height - radius, radius);
+}
+
+
 function draw() {
   background(220);
+  spawnShooter();
 }
