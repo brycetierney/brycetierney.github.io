3b4a5b14467ddea1b17d742879879ebbacf3f62a
brycetierney <161519956+brycetierney@users.noreply.github.com>
2024-04-10 14:12:26 -0600

    Update sketch.js

--- 2D grid/sketch.js
+++ 2D grid/sketch.js
@@ -19,19 +19,48 @@ const GRID_SIZE = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  //if randomizing the grid, do this:
   grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  
+  //this is dumb -- should check if this is the right size!
   cellSize = height/grid.length;
 }
 
 function draw() {
   background(220);
   displayGrid();
-  generateRandomGrid();
 }
 
 function keyPressed() {
   if (key === "r") {
     grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+  if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  // console.log(x, y);
+
+  //don't fall off the edge of the grid...
+  if (x < GRID_SIZE && y < GRID_SIZE) {
+    toggleCell(x, y);
+  }
+}
+
+function toggleCell(x, y) {
+  //toggle the color of the cell
+  if (grid[y][x] === 0) {
+    grid[y][x] = 1;
+  }
+  else {
+    grid[y][x] = 0;
   }
 }
 
@@ -39,10 +68,10 @@ function displayGrid() {
   for (let y = 0; y < grid.length; y++) {
     for (let x = 0; x < grid[y].length; x++) {
       if (grid[y][x] === 1) {
-        fill ("black");
+        fill("black");
       }
       else {
-        fill ("white");
+        fill("white");
       }
       square(x * cellSize, y * cellSize, cellSize);
     }
@@ -52,14 +81,26 @@ function displayGrid() {
 function generateRandomGrid(cols, rows) {
   let emptyArray = [];
   for (let y = 0; y < rows; y++) {
-    emptyArray.push([]); 
+    emptyArray.push([]);
     for (let x = 0; x < cols; x++) {
+      //half the time, be a 1. Other half, be a 0.
       if (random(100) < 50) {
-        emptyArray[y]. push(0);
+        emptyArray[y].push(0);
       }
       else {
         emptyArray[y].push(1);
       }
+    }
+  }
+  return emptyArray;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let emptyArray = [];
+  for (let y = 0; y < rows; y++) {
+    emptyArray.push([]);
+    for (let x = 0; x < cols; x++) {
+      emptyArray[y].push(0);
     }
   }
   return emptyArray;
