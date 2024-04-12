2f0136164c59be0cf751d206a9b78e660053f9ed
brycetierney <161519956+brycetierney@users.noreply.github.com>
2024-04-12 14:11:45 -0600

    added game of life

---
+++ Game-of-life/sketch.js
@@ -0,0 +1,184 @@
+// Project Title
+// Bryce Tierney
+// Apr 9, 2024
+
+// If hardcoding a level, use this:
+
+// let grid = [[1, 0, 0, 1],
+//   [0, 1, 0, 1],
+//   [0, 0, 0, 1],
+//   [1, 1, 0, 0],
+//   [1, 0, 1, 1],
+//   [0, 0, 0, 1]];
+
+// If randomizing grid, use this:
+
+let grid;
+let cellSize;
+const GRID_SIZE = 10;
+let toggleStyle = "self";
+let isAutoPlayOn = false;
+
+function setup() {
+  //make the canvas the largest square that you can...
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else {
+    createCanvas(windowHeight, windowHeight);
+  }
+
+  //if randomizing the grid, do this:
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  
+  //this is dumb -- should check if this is the right size!
+  cellSize = height/grid.length;
+}
+
+function windowResized() {
+  //make the canvas the largest square that you can...
+  if (windowWidth < windowHeight) {
+    resizeCanvas(windowWidth, windowWidth);
+  }
+  else {
+    resizeCanvas(windowHeight, windowHeight);
+  }
+
+  cellSize = height/grid.length;
+}
+
+function draw() {
+  background(220);
+  displayGrid();
+}
+
+function keyPressed() {
+  if (key === "r") {
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+  if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+  if (key === "n") {
+    toggleStyle = "neighbours";
+  }
+
+  if (key === "s") {
+    toggleStyle = "self";
+  }
+
+  if (key === " ") {
+    updateGrid();
+  }
+}
+
+
+function updateGrid() {
+
+  let nextTurn = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+
+
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      let neighbours = 0;
+
+
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+
+          if (x+i >= 0 && x+i < GRID_SIZE && y +j >= 0 && y+j < GRID_SIZE) {
+            neighbours += grid[x + i][y + j];
+          }
+        }
+      }
+    }
+
+
+    neighbours -= grid[y][x];
+
+
+
+
+    if (grid[y][x] === 1) {
+      nextTurn
+    }
+
+  }
+}
+
+
+
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  //toggle self
+  toggleCell(x, y);
+
+  // and NESW neighbours, if style is set to neighbours
+  if (toggleStyle === "neighbours") {
+    toggleCell(x + 1, y);
+    toggleCell(x - 1, y);
+    toggleCell(x, y + 1);
+    toggleCell(x, y - 1);
+  }
+}
+
+function toggleCell(x, y) {
+  // make sure the cell you're toggling is in the grid...
+  if (x < GRID_SIZE && y < GRID_SIZE &&
+      x >= 0 && y >= 0) {
+    //toggle the color of the cell
+    if (grid[y][x] === 0) {
+      grid[y][x] = 1;
+    }
+    else {
+      grid[y][x] = 0;
+    }
+  }
+}
+
+function displayGrid() {
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] === 1) {
+        fill("black");
+      }
+      else {
+        fill("white");
+      }
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
+
+function generateRandomGrid(cols, rows) {
+  let emptyArray = [];
+  for (let y = 0; y < rows; y++) {
+    emptyArray.push([]);
+    for (let x = 0; x < cols; x++) {
+      //half the time, be a 1. Other half, be a 0.
+      if (random(100) < 50) {
+        emptyArray[y].push(0);
+      }
+      else {
+        emptyArray[y].push(1);
+      }
+    }
+  }
+  return emptyArray;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let emptyArray = [];
+  for (let y = 0; y < rows; y++) {
+    emptyArray.push([]);
+    for (let x = 0; x < cols; x++) {
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
+}
