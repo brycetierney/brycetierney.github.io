1e653af60d4c3fc3da5aac2217aa6eee6f37e908
brycetierney <161519956+brycetierney@users.noreply.github.com>
2024-04-19 13:33:25 -0600

    Update sketch.js

--- space shooter/sketch.js
+++ space shooter/sketch.js
@@ -5,30 +5,10 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let cx;
-let cy;
-let radius;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-
 function draw() {
   background(220);
-  spawnShooter();
-}
-function spawnShooter() {
-  cx = width/2;
-  cy = height - radius;
-  radius = 60;
-
-  circle(cx, cy, radius);
-  constrain(circle(width - radius, height - radius));
-}
-
-function keyPressed() {
-  if (keyIsPressed === "w") {
-    cx -= 10;
-  }
 }
